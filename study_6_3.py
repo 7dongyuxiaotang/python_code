e2ec22d3c1f1c45ac40b48aaec16ffdb6c70be65
7dongyuxiaotang <614028708@qq.com>
2021-06-03 23:52:57 +0800

    2021年6月3日学习Python的代码

---
+++ study_6_3.py
@@ -0,0 +1,41 @@
+# for循环
+# 语法
+# for 变量名 in 可迭代对象：列表 字典 字符串 元组 集合
+#     代码1
+# for x in "aaaaa":
+#   print(x)
+# dic={"k1":111,"k2":222}
+# for i in dic:
+#     print(i,dic[i])
+# for i in range(100):
+#     print("i")
+# range(10)
+# username="hh"
+# password="123"
+# count = 0
+# for i in range(3):
+#     inp_name=input("请输入你的帐号：")
+#     inp_word=input("请输入你的密码：")
+
+#     if (inp_name==username and inp_word==password):
+#         print("登录成功")
+#         break
+#     else:
+#         if i<2:
+#             print("帐号或者密码错误,请重新输入")
+#         continue
+# else:
+#     print("输入次数错误过多，退出系统")
+# print("系统退出")
+# for x in range(6):
+#     if x==4: continue
+#     print(x)
+# print(int(0b1010))
+# mag="hello world"
+# print(mag[0])
+# print(mag[-1])
+# print(mag[0:7:2])
+
+
+
+
